0250452b6dd63f13bf29f26243cb7540394e7e0c
wa1ki0g <70200814+wa1ki0g@users.noreply.github.com>
2021-04-17 16:03:42 +0800

    Update 运行单一实例.cpp

--- 基础技术/运行单一实例.cpp
+++ 基础技术/运行单一实例.cpp
@@ -1,3 +1,7 @@
+//程序判断的原理，用CreatMutex函数创建一个命名的互斥对象，如果对象调用成功
+//而且通过调用GetLastError函数获取的返回码为ERROR_ALREADY_EXISTS,则表示该
+//命名互斥对象存在，即程序重复运行。否则认为程序是首次运行。
+
 #include <stdio.h>
 #include <windows.h>
  BOOL IsAlready()
